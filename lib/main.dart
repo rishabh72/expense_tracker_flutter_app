0a2d77ad9d62ddae0733ae82dd802ceb7033c5d6
rishabh <sonirishabh018@gmail.com>
2022-01-03 08:15:16 +0530

    textScaleFactor

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,23 @@ import 'widgets/transaction_list.dart';
 import 'widgets/new_transaction.dart';
 import 'models/transaction.dart';
 
+//textScaleFactor
+//final curScaleFactor = MediaQuery.of(context).textScaleFactor;
+
+//textScaleFactor tells you by how much text output in the app should be scaled.
+// Users can change this in their mobile phone / device settings.
+// Depending on your app, you might want to consider using this piece of information when setting font sizes.
+
+//Consider this example:
+
+//Text('Always the same size!', style: TextStyle(fontSize: 20));
+
+//This text ALWAYS has a size of 20 device pixels, no matter what the user changed in his / her device settings.
+
+//Text('This changes!', style: TextStyle(fontSize: 20 * curScaleFactor));
+//This text on the other hand also has a size of 20 if the user didn't change anything in the settings (because textScaleFactor by default is 1).
+// But if changes were made, the font size of this text respects the user settings.
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
